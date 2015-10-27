5c5f5a6847d8fd45258ca92f54d9d3bcd6fcc648
clement <croussy>
2015-10-27 17:50:05 +0100

    add phone number

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026165111) do
+ActiveRecord::Schema.define(version: 20151012140724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20151026165111) do
     t.time     "take_over"
     t.float    "geocoding_accuracy"
     t.string   "country"
+    t.string   "phone_number"
     t.integer  "geocoding_level"
   end
 
