60867b28066cc593a49454d581973416bdcef4b7
croussy <clroussy@laposte.net>
2015-12-08 17:25:27 +0100

    Add fallbacks for english translation

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,9 @@ module Mapotempo
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # Fallbacks
+    config.i18n.fallbacks = {:'en-US' => :en, :'en-GB' => :en}
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
